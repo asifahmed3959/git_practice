c42ebd9797f0ca90a2de116675428b0686216677
asif.ahmed.apploye@gmail.com <asif.ahmed.apploye@gmail.com>
2020-08-13 11:31:36 +0600

    merged conflicts and all

--- base/serializers.py
+++ base/serializers.py
@@ -9,3 +9,17 @@ class ChildSerializer(serializers.ModelSerializer):
     class Meta:
         model = Child
         fields = ('id', 'first_name', 'last_name')
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    children = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Parent
+        fields = ('id', 'first_name', 'last_name', 'street_address', 'city', 'state', 'zip', 'children')
+
+    def get_children(self, user):
+        queryset = user.children.all()
+        serializer = ChildSerializer(queryset, many=True)
+        return serializer.data
+
--- base/views.py
+++ base/views.py
@@ -19,7 +19,29 @@ class UserModelViewSet(ModelViewSet):
     def get_object(self):
         obj = self.queryset.filter(id=self.kwargs.get('parent_id')).first()
 
+class ChildrenModelViewSet(ModelViewSet):
+    permission_classes = [AllowAny, ]
+    serializer_class = ChildSerializer
+    lookup_field = 'child_id'
+
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+
+        self._parent = Parent.objects.filter(id=self.kwargs.get('parent_id', None)).first()
+
+        if self._parent is None:
+            raise Http404
+
+    def get_object(self):
+        obj = self.get_queryset().filter(id=self.kwargs.get('child_id')).first()
+
         if obj is None:
             raise Http404
 
         return obj
+
+    def get_queryset(self):
+        return Child.objects.filter(parent_id=self.kwargs.get('parent_id'))
+
+    def perform_create(self, serializer):
+        serializer.save(parent=self._parent)
